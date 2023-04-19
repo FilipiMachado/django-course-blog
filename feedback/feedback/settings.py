71d87eb595e11a8282c781532392a567b3a08ff1
filipi <filipichip7@gmail.com>
2023-04-18 22:41:36 -0300

    finished upload files modukle

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/user-media/"
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
+    
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
 
-class CreateProfileView(View):
+""" class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES['user_image'])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        }) """
