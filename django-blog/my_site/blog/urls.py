ebb44d92df45710b8f3587192124e948290c744d
filipi <filipichip7@gmail.com>
2023-04-24 15:14:43 -0300

    adding class based views to blog project

--- django-blog/my_site/blog/urls.py
+++ django-blog/my_site/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),  # /posts/my-first-post
 ]
--- django-blog/my_site/blog/views.py
+++ django-blog/my_site/blog/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
 
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-def starting_page(request):
+
+""" def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts,
-    })
+    }) """
 
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = 'all_posts'
 
-def posts(request):
+""" def posts(request):
     all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts,
-    })
+    }) """
 
 
 def post_detail(request, slug):
