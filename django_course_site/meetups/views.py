c28c4ee2a9ca39b2824cbafe2810ec59650e3265
filipi <filipichip7@gmail.com>
2023-04-25 23:15:56 -0300

    polished organizer email

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -23,7 +23,7 @@ def meetup_details(request, meetup_slug):
                 user_email = registration_form.cleaned_data['email']
                 participant, _ = Participant.objects.get_or_create(email=user_email)
                 selected_meetup.participants.add(participant)
-                return redirect('confirm-registration')
+                return redirect('confirm-registration', meetup_slug=meetup_slug)
                 
         return render(request, 'meetups/meetup-details.html', {
               'meetup_found': True,
@@ -35,6 +35,9 @@ def meetup_details(request, meetup_slug):
             'meetup_found': False,
         })
 
-def confirm_registration(request):
-    return render(request, 'meetups/registration-success.html')
+def confirm_registration(request, meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(request, 'meetups/registration-success.html', {
+        'organizer_email': meetup.organizer_email
+    })
     
