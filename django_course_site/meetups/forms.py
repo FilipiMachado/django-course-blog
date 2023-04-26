7b9bc2aa4faa6cb924efa17555ca8f4ba298b8c7
filipi <filipichip7@gmail.com>
2023-04-25 22:59:44 -0300

    more form adjusts

--- django_course_site/meetups/forms.py
+++ django_course_site/meetups/forms.py
@@ -1,8 +1,4 @@
 from django import forms
 
-from .models import Participant
-
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = Participant
-        fields = ['email']
+class RegistrationForm(forms.Form):
+    email = forms.EmailField(label="Your Email")
