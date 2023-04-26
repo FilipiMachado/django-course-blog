02ef1d2517c5511f2c16bad5ab3223f95f1400cc
filipi <filipichip7@gmail.com>
2023-04-25 23:21:02 -0300

    optimized urls

--- django_course_site/django_course_site/urls.py
+++ django_course_site/django_course_site/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meetups.urls')),
+    path('', RedirectView.as_view(url="/meetups")),
+    path('meetups/', include('meetups.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("meetups/", views.index, name="all-meetups"),
-    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name="confirm-registration"),
-    path("meetups/<slug:meetup_slug>", views.meetup_details, name="meetup-detail")
+    path("", views.index, name="all-meetups"),
+    path('<slug:meetup_slug>/success', views.confirm_registration, name="confirm-registration"),
+    path("<slug:meetup_slug>", views.meetup_details, name="meetup-detail")
 ]
