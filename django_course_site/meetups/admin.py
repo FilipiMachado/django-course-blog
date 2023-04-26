f79187667f65592eeb17b0d85647c4379f71536b
filipi <filipichip7@gmail.com>
2023-04-25 21:08:02 -0300

    finished many-to-many relation on models

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location
 
 # Register your models here.
 
-admin.site.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    list_filter = ('location',)
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
